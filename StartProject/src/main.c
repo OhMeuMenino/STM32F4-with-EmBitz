7274274a29866a6c66a50e37f640ced505c617b5
OhMeuMenino <OhMeuMenino@users.noreply.github.com>
2017-05-19 00:14:52 +0100

    blink LED

--- StartProject/src/main.c
+++ StartProject/src/main.c
@@ -1,22 +1,35 @@
 /*
+** Start project. It includes all Standart Peripherals Library
 **
-**                           Main.c
-**
+** This projects configure the GPIO G13 to blink the LED3 (green)
 **
 **********************************************************************/
 /*
-   Last committed:     $Revision: 00 $
-   Last changed by:    $Author: $
-   Last changed date:  $Date:  $
-   ID:                 $Id:  $
+   Last changed by:    OhMeuMenino
+   Last changed date:  May 2017
 
 **********************************************************************/
 #include "stm32f4xx_conf.h"
 
 int main(void)
 {
-  while(1)
-  {
-  
-  }
+    /* GPIOG Peripheral clock enable */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
+
+    /* Configure the PIN13 on PORTG */
+    GPIO_InitTypeDef  PORTG;
+    PORTG.GPIO_Pin = GPIO_Pin_13;       // Pin 13
+    PORTG.GPIO_Mode = GPIO_Mode_OUT;    // Output
+    PORTG.GPIO_OType = GPIO_OType_PP;   // Output Type - Push-pull mode
+    PORTG.GPIO_Speed = GPIO_Speed_100MHz;
+    PORTG.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOG, &PORTG);
+
+    int i=1000000;
+    while(1)
+    {
+        GPIO_ToggleBits(GPIOG,13);
+        for(i=1000000;i>0;i--);
+    }
+
 }
